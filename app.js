3efa095331e1afcdc8f9211fbf063d562ccf5ca5
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-03 11:54:29 +0300

    3 modal yasash

--- app.js
+++ app.js
@@ -1,29 +1,12 @@
-const button = document.querySelector('button');
-const ul =  document.querySelector('ul')
-const iteam = document.querySelectorAll('li');
 
-button.addEventListener('click', ()=>{
-    // ul.innerHTML += '<li></li>'
-   const li =  document.createElement('li')
-   li.textContent = ('sumtheng new text 😊')
-   ul.prepend(li)
-   
+const showbtn = document.getElementById('show-btn');
+const modal = document.getElementById('modal')
+const closeBtn = document.getElementById('close-btn')
+
+showbtn.addEventListener('click', ()=>{
+    modal.classList.remove('hidden')
 })
 
-
-// iteam.forEach((iteam)=>{
-//     iteam.addEventListener('click',(e)=>{
-       
-//         // e.target.remove()
-//         console.log("clicked li");
-//         e.stopImmediatePropagation()
-        
-//     })
-// })
-
-
-ul.addEventListener('click', (e)=>{
-    if(e.target.nodeName == 'LI'){
-        e.target.remove()
-    }
+closeBtn.addEventListener('click', ()=>{
+    modal.classList.add('hidden')
 })
