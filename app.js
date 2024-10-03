a2fad406822e80051a6b0826afc9747f4218d0d8
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-03 15:30:52 +0300

    ,

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-
-const showbtn = document.getElementById('show-btn');
-const modal = document.getElementById('modal')
-const closeBtn = document.getElementById('close-btn')
-
-showbtn.addEventListener('click', ()=>{
-    modal.classList.remove('hidden')
-})
-
-closeBtn.addEventListener('click', ()=>{
-    modal.classList.add('hidden')
-})
+const signupForm  =document.querySelector('#signup-form');
