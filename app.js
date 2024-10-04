d87c53604ca36a977cb37ffd7588e13b3ba4c979
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-04 21:07:57 +0300

    ss

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const signupForm  =document.querySelector('#signup-form');
+const signupForm  = document.querySelector('#signup-form');
+const userName = document.querySelector('#username')
+
+signupForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    console.log(userName.value);
+    
+    
+    
+})
