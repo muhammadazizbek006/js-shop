9278ed501070f5d316c4de500f60fdfd815abefe
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-05 10:00:48 +0300

    x

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const movies =  [
-    {name:'avatar 3D', yeyar:2008, rating:9},
-    {name:'titanik 4D', yeyar:1997, rating:7},
-    {name:'forsaj 9 3D', yeyar:2020, rating:4},
-    {name:'avengers', yeyar:2012, rating:6},
+    {name:'avatar 3D', year:2008, rating:9},
+    {name:'titanik 4D', year:1997, rating:7},
+    {name:'forsaj 9 3D', year:2020, rating:4},
+    {name:'avengers', year:2012, rating:6},
 ]
-const newmowies = []
-movies.forEach((e)=>{
-    if(e.yeyar > 2010){
-        newmowies.push(e)
-    }
+// const newmowies = []
+// movies.forEach((e)=>{
+//     if(e.yeyar > 2010){
+//         newmowies.push(e)
+//     }
     
+// })
+
+// console.log(newmowies);
+
+const filtermovies = movies.filter((movie)=>{
+    return movie.year > 2010
 })
 
-console.log(newmowies);
+console.log(filtermovies);
+
