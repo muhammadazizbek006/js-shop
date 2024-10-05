1021b48eb1330b7f095ce5bb7e10decdddce4bed
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-05 13:22:11 +0300

    js set timout

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const movies =  [
-    {name:'avatar 3D', year:2008, rating:9},
-    {name:'titanik 4D', year:1997, rating:7},
-    {name:'forsaj 9 3D', year:2020, rating:4},
-    {name:'avengers', year:2012, rating:6},
-]
+setTimeout( () => {
+    console.log('helllo wrold');
+}, 3000 )
+
+
 
 
 
