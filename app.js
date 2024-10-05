9b2d452651375d71b410a12fbc76d309fb4d223e
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-05 09:51:10 +0300

    s

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-const signupForm  = document.querySelector('#signup-form');
-const userName = document.querySelector('#username')
-
-signupForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
-    console.log(userName.value);
-    
-    
-    
-})
+const movies =  [
+    {name:'avatar 3D', yeyar:2008, rating:9},
+    {name:'titanik 4D', yeyar:1997, rating:7},
+    {name:'forsaj 9 3D', yeyar:2020, rating:4},
+    {name:'avengers', yeyar:2012, rating:6},
+]
